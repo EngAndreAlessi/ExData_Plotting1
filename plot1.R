0a8cfed42efd68323d6890e75f2f010a40da6d03
Andre Alessi <andreedalessi@gmail.com>
2020-09-29 21:56:36 -0300

    Creates new column DateTime

--- plot1.R
+++ plot1.R
@@ -4,8 +4,8 @@ data <- read.table(unzip("exdata_data_household_power_consumption.zip"),
                    na.strings = "?")
 # Slice for the lines of interest
 data <- data[66637:69516, ] 
-data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
-data$Time <- strptime(data$Time, format = "%H:%M:%S")
+data$DataTime <- strptime(paste(data$Date, data$Time), 
+                          format = "%d/%m/%Y %H:%M:%S")
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 hist(data$Global_active_power, 
      xlab = "Global Active Power (kilowatts)", 
