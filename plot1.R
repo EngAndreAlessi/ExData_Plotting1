ead7fd6189ca81e29cd3f772adba1233b5cd0475
Andre Alessi <andreedalessi@gmail.com>
2020-09-29 22:07:31 -0300

    Fixed variable name from DataTime to DateTime

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@ data <- read.table(unzip("exdata_data_household_power_consumption.zip"),
                    na.strings = "?")
 # Slice for the lines of interest
 data <- data[66637:69516, ] 
-data$DataTime <- strptime(paste(data$Date, data$Time), 
+data$DateTime <- strptime(paste(data$Date, data$Time), 
                           format = "%d/%m/%Y %H:%M:%S")
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 hist(data$Global_active_power, 
--- plot2.R
+++ plot2.R
@@ -4,10 +4,10 @@ data <- read.table(unzip("exdata_data_household_power_consumption.zip"),
                    na.strings = "?")
 # Slice for the lines of interest
 data <- data[66637:69516, ] 
-data$DataTime <- strptime(paste(data$Date, data$Time), 
+data$DateTime <- strptime(paste(data$Date, data$Time), 
                           format = "%d/%m/%Y %H:%M:%S")
 png(filename = "plot2.png", width = 480, height = 480, units = "px")
-plot(data$DataTime,
+plot(data$DateTime,
      data$Global_active_power,
      type = "l",
      xlab = "",
